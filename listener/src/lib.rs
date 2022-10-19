e1512f51941ab2db4a7c0a4bfb0f5b54cd0f9d98
George Williamson <george@casperlabs.io>
2022-10-19 17:19:06 +0100

    Implements reconnect logic in listener crate

--- listener/src/lib.rs
+++ listener/src/lib.rs
@@ -1,14 +1,241 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod utils;
+
+use std::{fmt::Debug, time::Duration};
+
+use casper_event_types::SseData;
+use casper_types::ProtocolVersion;
+
+use anyhow::Error;
+use bytes::Bytes;
+use eventsource_stream::{EventStream, Eventsource};
+use reqwest::Client;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+use tokio_stream::{Stream, StreamExt};
+use tracing::{error, info, warn};
+
+use utils::resolve_address;
+
+const MAIN_FILTER_PATH: &str = "main";
+const DEPLOYS_FILTER_PATH: &str = "deploys";
+const SIGNATURES_FILTER_PATH: &str = "sigs";
+
+#[derive(Clone)]
+pub struct EventListener {
+    bind_address: String,
+    pub api_version: ProtocolVersion,
+    current_event_id: Option<u32>,
+    max_retries: u8,
+    delay_between_retries: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct SseEvent {
+    pub source: String,
+    pub id: Option<u32>,
+    pub data: SseData,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl EventListener {
+    /// Returns an instance of [EventListener] based on the given `bind_address`.  
+    /// `delay_between_retries` should be provided in seconds.
+    ///
+    /// Will error if:
+    /// - `bind_address` cannot be parsed into a network address.
+    /// - It is unable to connect to the source.
+    /// - The source doesn't send a valid [ApiVersion] as the first event.
+    pub async fn new(
+        bind_address: String,
+        max_retries: u8,
+        delay_between_retries: u8,
+    ) -> Result<Self, Error> {
+        resolve_address(&bind_address)?;
+        let prefixed_address = format!("http://{}", bind_address);
+
+        let address_with_filter = format!("{}/events/main", prefixed_address);
+        let stream = Client::new()
+            .get(&address_with_filter)
+            .send()
+            .await?
+            .bytes_stream()
+            .eventsource();
+
+        let (api_version, _) = parse_api_version(stream).await?;
+
+        Ok(Self {
+            bind_address: prefixed_address,
+            api_version,
+            current_event_id: None,
+            max_retries,
+            delay_between_retries,
+        })
+    }
+
+    pub async fn consume_combine_streams(&self) -> UnboundedReceiver<SseEvent> {
+        let (sse_tx, sse_rx) = unbounded_channel::<SseEvent>();
+
+        tokio::spawn(connect_with_retry(
+            sse_tx.clone(),
+            self.bind_address.clone(),
+            MAIN_FILTER_PATH.to_string(),
+            self.current_event_id,
+            self.max_retries,
+            self.delay_between_retries,
+        ));
+
+        tokio::spawn(connect_with_retry(
+            sse_tx.clone(),
+            self.bind_address.clone(),
+            DEPLOYS_FILTER_PATH.to_string(),
+            self.current_event_id,
+            self.max_retries,
+            self.delay_between_retries,
+        ));
+
+        tokio::spawn(connect_with_retry(
+            sse_tx,
+            self.bind_address.clone(),
+            SIGNATURES_FILTER_PATH.to_string(),
+            self.current_event_id,
+            self.max_retries,
+            self.delay_between_retries,
+        ));
+
+        sse_rx
+    }
+}
+
+async fn connect_with_retry(
+    event_sender: UnboundedSender<SseEvent>,
+    bind_address: String,
+    filter: String,
+    start_from_id: Option<u32>,
+    max_retries: u8,
+    delay_between: u8,
+) {
+    let mut retry_count = 0;
+    let mut current_event_id = start_from_id;
+
+    while retry_count <= max_retries {
+        if retry_count > 0 {
+            info!(
+                bind_address,
+                filter, "Attempting to reconnect... ({}/{})", retry_count, max_retries
+            );
+        }
+
+        let mut url = format!("{}/events/{}", bind_address, filter);
+        if let Some(event_id) = current_event_id {
+            let start_from_query = format!("?start_from={}", event_id);
+            url.push_str(&start_from_query);
+        }
+
+        let (api_version, mut event_stream) = match connect(url).await {
+            Ok(ok_val) => {
+                // reset retry counter since the connection was successful
+                retry_count = 0;
+                ok_val
+            }
+            Err(_) => {
+                // increment retry counter since the connection failed
+                retry_count += 1;
+                // wait the configured delay before continuing
+                tokio::time::sleep(Duration::from_secs(delay_between as u64)).await;
+                continue;
+            }
+        };
+
+        info!(%bind_address, %filter, %api_version, "Connected to event stream{}", {
+            if let Some(event_id) = start_from_id {
+                format!(", resuming from id: {}", event_id)
+            } else {
+                "".to_string()
+            }
+        });
+
+        while let Some(event) = event_stream.next().await {
+            match event {
+                Ok(event) => {
+                    let event_id: u32 = event.id.parse().expect("Error parsing id into u32");
+                    current_event_id = Some(event_id);
+
+                    let cloned_sender = event_sender.clone();
+
+                    match serde_json::from_str::<SseData>(&event.data) {
+                        Ok(SseData::Shutdown) | Err(_) => {
+                            error!(
+                                %bind_address,
+                                %filter,
+                                "Error connecting, retrying in {}s",
+                                delay_between
+                            );
+                            retry_count += 1;
+                            tokio::time::sleep(Duration::from_secs(delay_between as u64)).await;
+                            break;
+                        }
+                        Ok(sse_data) => {
+                            let _ = cloned_sender.send(SseEvent {
+                                source: bind_address.clone(),
+                                id: Some(event_id),
+                                data: sse_data,
+                            });
+                        }
+                    }
+                }
+                Err(event_stream_err) => {
+                    warn!(%bind_address, %filter, message = "Error returned from event stream", error=?event_stream_err)
+                }
+            }
+        }
+    }
+
+    // Retries have been exhausted without a successful connection - send Shutdown.
+    // Having tried to reconnect and failed we send the Shutdown.
+    let _ = event_sender.send(SseEvent {
+        source: bind_address,
+        id: None,
+        data: SseData::Shutdown,
+    });
+}
+
+async fn connect(
+    url: String,
+) -> Result<
+    (
+        ProtocolVersion,
+        EventStream<impl Stream<Item = reqwest::Result<Bytes>> + Sized>,
+    ),
+    Error,
+> {
+    let event_stream = Client::new()
+        .get(&url)
+        .send()
+        .await?
+        .bytes_stream()
+        .eventsource();
+
+    // Parse the first event to see if the connection was successful
+    let (api_version, event_stream) = parse_api_version(event_stream).await?;
+
+    Ok((api_version, event_stream))
+}
+
+async fn parse_api_version<EvtStr, E>(
+    mut stream: EventStream<EvtStr>,
+) -> Result<(ProtocolVersion, EventStream<EvtStr>), Error>
+where
+    E: Debug,
+    EvtStr: Stream<Item = Result<Bytes, E>> + Sized + Unpin,
+{
+    match stream.next().await {
+        None => Err(Error::msg("First event was empty")),
+        Some(Err(error)) => Err(Error::msg(format!(
+            "failed to get first event: {:?}",
+            error
+        ))),
+        Some(Ok(event)) => match serde_json::from_str::<SseData>(&event.data) {
+            Ok(SseData::ApiVersion(api_version)) => Ok((api_version, stream)),
+            Ok(_) => Err(Error::msg("First event should have been API version")),
+            Err(serde_err) => Err(Error::from(serde_err)
+                .context("First event could not be deserialized into ApiVersion")),
+        },
     }
 }
---
+++ listener/src/utils.rs
@@ -0,0 +1,54 @@
+use std::{
+    fmt::{self, Debug, Display, Formatter},
+    io,
+    net::{SocketAddr, ToSocketAddrs},
+};
+
+use thiserror::Error;
+
+/// DNS resolution error.
+#[derive(Debug, Error)]
+#[error("could not resolve `{address}`: {kind}")]
+pub struct ResolveAddressError {
+    /// Address that failed to resolve.
+    address: String,
+    /// Reason for resolution failure.
+    kind: ResolveAddressErrorKind,
+}
+
+/// DNS resolution error kind.
+#[derive(Debug)]
+enum ResolveAddressErrorKind {
+    /// Resolve returned an error.
+    ErrorResolving(io::Error),
+    /// Resolution did not yield any address.
+    NoAddressFound,
+}
+
+impl Display for ResolveAddressErrorKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            ResolveAddressErrorKind::ErrorResolving(err) => {
+                write!(f, "could not run dns resolution: {}", err)
+            }
+            ResolveAddressErrorKind::NoAddressFound => {
+                write!(f, "no addresses found")
+            }
+        }
+    }
+}
+
+/// Parses a network address from a string, with DNS resolution.
+pub(crate) fn resolve_address(address: &str) -> Result<SocketAddr, ResolveAddressError> {
+    address
+        .to_socket_addrs()
+        .map_err(|err| ResolveAddressError {
+            address: address.to_string(),
+            kind: ResolveAddressErrorKind::ErrorResolving(err),
+        })?
+        .next()
+        .ok_or_else(|| ResolveAddressError {
+            address: address.to_string(),
+            kind: ResolveAddressErrorKind::NoAddressFound,
+        })
+}
