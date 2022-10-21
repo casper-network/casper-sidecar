cc8825a0c91855c07c24100c0b479c9987e70502
George Williamson <george@casperlabs.io>
2022-10-21 13:41:01 +0100

    Adds Rate and Concurrency limiting to Rest Server

--- sidecar/src/rest_server.rs
+++ sidecar/src/rest_server.rs
@@ -4,7 +4,14 @@ mod handlers;
 #[cfg(test)]
 mod tests;
 
+use std::net::TcpListener;
+use std::time::Duration;
+
 use anyhow::Error;
+use hyper::Server;
+use tower::buffer::Buffer;
+use tower::limit::RateLimitLayer;
+use tower::{limit::ConcurrencyLimitLayer, make::Shared, ServiceBuilder};
 
 use crate::{sqlite_database::SqliteDatabase, utils::resolve_address};
 
@@ -16,7 +23,17 @@ pub async fn run_server(port: u16, sqlite_database: SqliteDatabase) -> Result<()
     let address = format!("{}:{}", LOOPBACK, port);
     let socket_address = resolve_address(&address)?;
 
-    warp::serve(api).run(socket_address).await;
+    let listener = TcpListener::bind(socket_address)?;
+
+    let warp_service = warp::service(api);
+    let tower_service = ServiceBuilder::new()
+        .layer(ConcurrencyLimitLayer::new(50))
+        .layer(RateLimitLayer::new(50, Duration::from_secs(1)))
+        .service(warp_service);
+
+    Server::from_tcp(listener)?
+        .serve(Shared::new(Buffer::new(tower_service, 50)))
+        .await?;
 
     Ok(())
 }
