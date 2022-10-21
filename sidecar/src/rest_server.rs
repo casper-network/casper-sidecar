d40888e886a141625b39b1197b0f63f655d410d0
George Williamson <george@casperlabs.io>
2022-10-21 14:41:50 +0100

    Makes limits configurable, adds request timeout

--- sidecar/src/rest_server.rs
+++ sidecar/src/rest_server.rs
@@ -13,22 +13,35 @@ use tower::buffer::Buffer;
 use tower::limit::RateLimitLayer;
 use tower::{limit::ConcurrencyLimitLayer, make::Shared, ServiceBuilder};
 
-use crate::{sqlite_database::SqliteDatabase, utils::resolve_address};
+use crate::{
+    sqlite_database::SqliteDatabase, types::config::RestServerConfig, utils::resolve_address,
+};
 
 const LOOPBACK: &str = "127.0.0.1";
 
-pub async fn run_server(port: u16, sqlite_database: SqliteDatabase) -> Result<(), Error> {
+pub async fn run_server(
+    config: RestServerConfig,
+    sqlite_database: SqliteDatabase,
+) -> Result<(), Error> {
     let api = filters::combined_filters(sqlite_database);
 
-    let address = format!("{}:{}", LOOPBACK, port);
+    let address = format!("{}:{}", LOOPBACK, config.port);
     let socket_address = resolve_address(&address)?;
 
     let listener = TcpListener::bind(socket_address)?;
 
     let warp_service = warp::service(api);
     let tower_service = ServiceBuilder::new()
-        .layer(ConcurrencyLimitLayer::new(50))
-        .layer(RateLimitLayer::new(50, Duration::from_secs(1)))
+        .layer(ConcurrencyLimitLayer::new(
+            config.max_concurrent_requests as usize,
+        ))
+        .layer(RateLimitLayer::new(
+            config.max_requests_per_second as u64,
+            Duration::from_secs(1),
+        ))
+        .timeout(Duration::from_secs(
+            config.request_timeout_in_seconds as u64,
+        ))
         .service(warp_service);
 
     Server::from_tcp(listener)?
