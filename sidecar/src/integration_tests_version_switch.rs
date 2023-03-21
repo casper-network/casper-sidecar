bc6f55a763b7a949d36176cdcfc1e3cde191911f
Jakub Zajkowski <jakub@casperlabs.io>
2023-03-21 14:33:42 +0100

    Hardening ITs

--- sidecar/src/integration_tests_version_switch.rs
+++ sidecar/src/integration_tests_version_switch.rs
@@ -48,22 +48,24 @@ pub mod tests {
         assert!(events_received.get(4).unwrap().contains("\"Shutdown\""));
     }
 
-    #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+    #[tokio::test(flavor = "multi_thread", worker_threads = 10)]
     async fn polling_latest_data_should_return_latest_api_version() {
         let (node_port_for_sse_connection, node_port_for_rest_connection, event_stream_server_port) =
             start_sidecar().await;
         let shutdown_tx = sse_server_example_data_1_0_0(node_port_for_sse_connection).await;
         let change_api_version_tx = status_1_0_0_server(node_port_for_rest_connection);
-        thread::sleep(time::Duration::from_secs(5)); //give some time for sidecar to connect and data to propagate
-        shutdown_tx.send(()).unwrap();
         change_api_version_tx
             .send("1.4.10".to_string())
             .await
             .unwrap();
+        shutdown_tx.send(()).unwrap();
+        thread::sleep(time::Duration::from_secs(3)); //give some time everything to disconnect
         let main_event_stream = connect_to_sidecar("/events/main", event_stream_server_port).await;
         let shutdown_tx = sse_server_example_data_1_4_10(node_port_for_sse_connection).await;
-        thread::sleep(time::Duration::from_secs(4)); //give some time everything to disconnect
-        shutdown_tx.send(()).unwrap();
+        tokio::spawn(async move{
+            thread::sleep(time::Duration::from_secs(4)); //give some time to poll the data
+            shutdown_tx.send(()).unwrap();
+        });
         let events_received = poll_events(main_event_stream).await;
         assert_eq!(events_received.len(), 3);
         assert!(events_received.get(0).unwrap().contains("\"1.4.10\""));
