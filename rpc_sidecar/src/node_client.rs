a2801e82e117a6b57ada2221fb3bce24136c7612
Rafał Chabowski <rafal@casperlabs.io>
2024-06-06 17:02:12 +0200

    Satisfy clippy

--- rpc_sidecar/src/node_client.rs
+++ rpc_sidecar/src/node_client.rs
@@ -1149,13 +1149,13 @@ mod tests {
         let port = get_port();
         let config = NodeClientConfig::new_with_port(port);
         let shutdown = Arc::new(tokio::sync::Notify::new());
-        let _ = start_mock_binary_port(port, vec![], 1, Arc::clone(&shutdown)).await;
+        let _mock_server_handle = start_mock_binary_port(port, vec![], 1, Arc::clone(&shutdown)).await;
         let (c, _) = FramedNodeClient::new(config).await.unwrap();
 
         let generated_ids: Vec<_> = (INITIAL_REQUEST_ID..INITIAL_REQUEST_ID + 10)
             .map(|_| {
                 let (_, binary_message) = c.generate_payload(get_dummy_request());
-                let header = BinaryRequestHeader::from_bytes(&binary_message.payload())
+                let header = BinaryRequestHeader::from_bytes(binary_message.payload())
                     .unwrap()
                     .0;
                 header.id()
