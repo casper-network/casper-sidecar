2fd2d42c4f19f08f272019287c5ead826c1e4fc8
Jakub Zajkowski <ja.zajko@gmail.com>
2025-04-10 14:28:07 +0200

    WIP

--- event_sidecar/src/tests/integration_tests.rs
+++ event_sidecar/src/tests/integration_tests.rs
@@ -235,7 +235,6 @@ async fn should_respond_to_rest_query() {
         node_port_for_rest_connection,
         event_stream_server_port,
     ) = build_test_config();
-
     let sidecar_rest_server_port = testing_config.rest_api_server_config.port;
     let mut node_mock = MockNodeBuilder::build_example_2_0_0_node(
         node_port_for_sse_connection,
@@ -246,6 +245,7 @@ async fn should_respond_to_rest_query() {
     let (_, receiver) =
         fetch_data_from_endpoint("/events?start_from=0", event_stream_server_port).await;
     wait_for_n_messages(1, receiver, Duration::from_secs(30)).await;
+
     let block_request_url = format!("http://127.0.0.1:{sidecar_rest_server_port}/block");
     let response = reqwest::Client::new()
         .get(&block_request_url)
